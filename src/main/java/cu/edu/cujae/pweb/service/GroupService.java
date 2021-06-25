d40840aa2c75354abf256c8c850507c40c7fd484
gdezayas <gdzayas@gmail.com>
2021-06-24 22:41:32 -0400

    Crear GroupServices

---
+++ src/main/java/cu/edu/cujae/pweb/service/GroupService.java
@@ -0,0 +1,12 @@
+package cu.edu.cujae.pweb.service;
+
+import cu.edu.cujae.pweb.dto.GroupDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface GroupService {
+    List<GroupDto> getGroupList() throws SQLException;
+    void createGroup(GroupDto group) throws SQLException;
+    void deleteGroup(int codGroup) throws SQLException;
+}
---
+++ src/main/java/cu/edu/cujae/pweb/service/GroupServiceImpl.java
@@ -0,0 +1,57 @@
+package cu.edu.cujae.pweb.service;
+
+import cu.edu.cujae.pweb.dto.GroupDto;
+import cu.edu.cujae.pweb.dto.UserDto;
+import cu.edu.cujae.pweb.security.CurrentUserUtils;
+import cu.edu.cujae.pweb.utils.ApiRestMapper;
+import cu.edu.cujae.pweb.utils.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GroupServiceImpl  implements GroupService{
+    @Autowired
+    private RestService restService;
+    @Override
+    public List<GroupDto> getGroupList() {
+        List<GroupDto> groupList = new ArrayList<GroupDto>();
+        try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            ApiRestMapper<GroupDto> apiRestMapper = new ApiRestMapper<>();
+            String response = (String)restService.GET("/api/v1/groups/", params, String.class, CurrentUserUtils.getTokenBearer()).getBody();
+            groupList = apiRestMapper.mapList(response, GroupDto.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return groupList;
+    }
+
+    @Override
+    public void createGroup(GroupDto group){
+        try{
+            restService.POST("/api/v1/groups/",group,String.class,CurrentUserUtils.getTokenBearer()).getBody();
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void deleteGroup(int codGroup) {
+        try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            UriTemplate template = new UriTemplate("/api/v1/groups/{codGroup}");
+            String uri = template.expand(codGroup).toString();
+            restService.DELETE(uri,params,String.class,CurrentUserUtils.getTokenBearer()).getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
