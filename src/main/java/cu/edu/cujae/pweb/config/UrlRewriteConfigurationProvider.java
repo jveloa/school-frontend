9cd395bd708901640124fd2e89281bd2a1214928
Junior Velo <juniorvelo4@gmail.com>
2021-06-30 02:30:55 -0400

    arreglar url

--- src/main/java/cu/edu/cujae/pweb/config/UrlRewriteConfigurationProvider.java
+++ src/main/java/cu/edu/cujae/pweb/config/UrlRewriteConfigurationProvider.java
@@ -22,7 +22,7 @@ public class UrlRewriteConfigurationProvider extends HttpConfigurationProvider {
             .addRule(Join.path("/evalution").to("/pages/security/notas/nota-list.jsf"))
             .addRule(Join.path("/groups").to("/pages/security/group/group-list.jsf"))
             .addRule(Join.path("/students_by_groups").to("/pages/security/reports/student-by-group/student-by-group.jsf"))
-            .addRule(Join.path("/subjects_by_years").to("/pages/security/reports/subjects-by-year/subjects-by-year.jsf"))
+            .addRule(Join.path("/subject_by_years").to("/pages/security/reports/subjects-by-year/subjects-by-year.jsf"))
             .addRule(Join.path("/evaluations_by_groups").to("/pages/security/reports/evaluations-by-group/evaluations-by-group.jsf"))
             .addRule(Join.path("/student_ladder").to("/pages/security/reports/student-ladder/student-ladder.jsf"));
     }
